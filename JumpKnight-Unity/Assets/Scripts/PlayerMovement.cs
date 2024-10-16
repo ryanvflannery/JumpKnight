6ff2fd23b43129e1a1396770fda4d6bd5e9b96ab
ryanvflannery <ryanvflannery@gmail.com>
2024-10-15 17:11:44 -0700

    Added Idle Player Animation

--- JumpKnight-Unity/Assets/Scripts/PlayerMovement.cs
+++ JumpKnight-Unity/Assets/Scripts/PlayerMovement.cs
@@ -15,11 +15,23 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+
         // assigning speed in all directions left, right, up, and down
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        body.velocity = new Vector2(horizontalInput* speed, body.velocity.y);
+
+        // Flip player when moving left and right
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
 
         // if player presses space bar they will jump
         if (Input.GetKey(KeyCode.Space))
+            // When space is pressed we will maintain the current velocity on the x axis
+            // This will apply a velocity of 10 on the y axis
             body.velocity = new Vector2(body.velocity.x, speed);
     }
 }
