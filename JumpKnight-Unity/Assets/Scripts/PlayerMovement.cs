f1758a9da4c46473fe830acfc67d41e61f28594b
ryanvflannery <ryanvflannery@gmail.com>
2024-11-19 23:30:42 -0800

    Finished Player Animations

--- JumpKnight-Unity/Assets/Scripts/PlayerMovement.cs
+++ JumpKnight-Unity/Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,20 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] private float speed;
-
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
 
     private void Awake()
     {
+        // Grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
-
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-
 
         // assigning speed in all directions left, right, up, and down
         body.velocity = new Vector2(horizontalInput* speed, body.velocity.y);
@@ -29,9 +30,25 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
 
         // if player presses space bar they will jump
-        if (Input.GetKey(KeyCode.Space))
-            // When space is pressed we will maintain the current velocity on the x axis
-            // This will apply a velocity of 10 on the y axis
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        // Set animator parameters left or right
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        // When space is pressed we will maintain the current velocity on the x axis 
+        body.velocity = new Vector2(body.velocity.x, speed); // This will apply a velocity of 10 on the y axis
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
